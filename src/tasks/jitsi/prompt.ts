cb75748b16c3a35099948d1ba935500135c42945
Ludovic Muller <ludovicmuller1@gmail.com>
2021-01-15 10:42:46 +0100

    task(jitsi/prompt): implement retries

--- src/tasks/jitsi/prompt.ts
+++ src/tasks/jitsi/prompt.ts
@@ -1,25 +1,39 @@
-import { By, Key } from 'selenium-webdriver';
+import { By, Key, WebElement } from 'selenium-webdriver';
 import { DISPLAY_NAME_INPUT } from '../../lib/jitsi/css';
+import { wait } from '../../lib/time';
 import DefaultTask from '../default';
 import { TaskParams } from '../task';
 
 /**
  * Autofill display name on Jitsi Meet prompt.
  *
- * @param {boolean} [required=true] `true` will make the task fail if there is no prejoin page
- * @param {string} [displayName] the display name ; if no one is specified, one will be generated
+ * @param {number} [retries=20] number of allowed retries.
+ * @param {number} [interval=500] interval in milliseconds between each retry.
+ * @param {boolean} [required=true] `true` will make the task fail if there is no prejoin page.
+ * @param {string} [displayName] the display name ; if no one is specified, one will be generated.
  */
 class JitsiPromptTask extends DefaultTask {
   async run(params?: TaskParams): Promise<void> {
     await super.run(params);
 
+    const retries = this.getNumericArg('retries', 5);
+    const interval = this.getNumericArg('interval', 500);
     const required = this.getBooleanArg('required', true);
     const displayName = this.getStringArg('displayName', `${this.args.browser.name} (jitsi-tests)`);
 
     // find the displayName input
-    const displayNameInput = await this.args.driver.findElements(
-      By.css(DISPLAY_NAME_INPUT),
-    );
+    let displayNameInput: WebElement[] = [];
+    for (let i = 0; i < retries; i += 1) {
+      displayNameInput = await this.args.driver.findElements(
+        By.css(DISPLAY_NAME_INPUT),
+      );
+
+      if (displayNameInput.length < 1) {
+        await wait(interval);
+      } else {
+        break;
+      }
+    }
 
     // try to fill the displayName input
     if (displayNameInput.length > 0) {
