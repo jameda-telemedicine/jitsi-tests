faafe937a3efc2a620f828ea83f93ad638631a2e
Ludovic Muller <ludovicmuller1@gmail.com>
2021-03-18 20:50:57 +0100

    screenshare: toggle

--- src/lib/jitsi/translations.ts
+++ src/lib/jitsi/translations.ts
@@ -3,3 +3,4 @@
 export const HANGUP_BUTTON = 'toolbar.accessibilityLabel.hangup';
 export const MUTE_AUDIO = 'toolbar.accessibilityLabel.mute';
 export const MUTE_VIDEO = 'toolbar.accessibilityLabel.videomute';
+export const SCREEN_SHARE = 'toolbar.accessibilityLabel.shareYourScreen';
---
+++ src/tasks/jitsi/screenshare.ts
@@ -0,0 +1,31 @@
+import { By, WebElement } from 'selenium-webdriver';
+import { TOOLBOX_BUTTON } from '../../lib/jitsi/css';
+import { SCREEN_SHARE } from '../../lib/jitsi/translations';
+import { waitSeconds } from '../../lib/time';
+import DefaultTask from '../default';
+import { TaskParams } from '../task';
+
+class JitsiScreenshareTask extends DefaultTask {
+  screenshareButton?: WebElement;
+
+  async toogleScreenshare(): Promise<void> {
+    await this.args.driver.executeScript('arguments[0].click()', this.screenshareButton);
+    await waitSeconds(5);
+  }
+
+  async run(params?: TaskParams): Promise<void> {
+    await super.run(params);
+
+    const screenshareText = await this.args.driver.executeScript(
+      `return $.i18n.t('${SCREEN_SHARE}');`,
+    );
+
+    this.screenshareButton = await this.args.driver
+      .findElement(By.css(`${TOOLBOX_BUTTON}[aria-label="${screenshareText}"]`));
+
+    await this.toogleScreenshare();
+    await this.toogleScreenshare();
+  }
+}
+
+export default JitsiScreenshareTask;
