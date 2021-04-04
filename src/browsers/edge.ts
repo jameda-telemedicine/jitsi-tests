c427c24c669ae1d8e686217e43e057fcf1064cfa
Ludovic Muller <ludovicmuller1@gmail.com>
2021-04-04 21:51:30 +0200

    browsers/edge: no need to override Options type anymore

--- src/browsers/edge.ts
+++ src/browsers/edge.ts
@@ -1,4 +1,3 @@
-import type { Options } from 'selenium-webdriver/chrome';
 import { Builder, Capabilities } from 'selenium-webdriver';
 import browser from 'selenium-webdriver/edge';
 import { BrowserConfig } from '../types/browsers';
@@ -7,7 +6,7 @@ import { builderWithCapabilities } from '../utils/driver';
 const browserName = 'MicrosoftEdge';
 
 const fetchBuilder = (config: BrowserConfig, capabilities?: Capabilities): Builder => {
-  const options = new browser.Options() as Options;
+  const options = new browser.Options();
   options.addArguments(
     'use-fake-device-for-media-stream',
     'use-fake-ui-for-media-stream',
