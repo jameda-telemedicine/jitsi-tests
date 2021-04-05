cbe96026b1c0b4fd6ce5c5289f6e58627fe80255
Ludovic Muller <ludovicmuller1@gmail.com>
2021-04-05 14:12:25 +0200

    create jitsi/call/end task

---
+++ src/tasks/jitsi/call/end.ts
@@ -0,0 +1,19 @@
+import { HANGUP_BUTTON } from '../../../lib/jitsi/translations';
+import { TaskParams } from '../../task';
+import JitsiTask from '../jitsi';
+
+class JitsiCallEndTask extends JitsiTask {
+  async run(params?: TaskParams): Promise<void> {
+    await super.run(params);
+
+    const endCallText = await this.getJitsiTranslation(HANGUP_BUTTON);
+    const hangupButton = await this.getJitsiToolboxButton(endCallText);
+
+    await this.args.driver.executeScript(
+      'arguments[0].click()',
+      hangupButton,
+    );
+  }
+}
+
+export default JitsiCallEndTask;
--- src/tasks/jitsi/hangup.ts
+++ src/tasks/jitsi/hangup.ts
@@ -2,6 +2,9 @@ import { HANGUP_BUTTON } from '../../lib/jitsi/translations';
 import { TaskParams } from '../task';
 import JitsiTask from './jitsi';
 
+/**
+ * @deprecated will be replaced with jitsi/call/end
+ */
 class JitsiHangupTask extends JitsiTask {
   async run(params?: TaskParams): Promise<void> {
     await super.run(params);
